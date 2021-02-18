97ccb935d1adaa4ba960ecafd1f46f313b8d4a9f
suhcrates <suhcrates@hanmail.net>
2021-02-19 01:16:54 +0900

    asdf

---
+++ timeline.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+from chul_ma import chulbal, magam
+# import article
+# from make_dict import make_dict, magam_check, yon_data
+# import post
+import time
+from telebot import bot
+
+def timeline():
+    i=0
+    while True:
+        now= datetime.today().strftime(format='%H:%M')
+        print('1/3')
+        #출발
+        if (now >= "09:00") and (now<="09:05"):
+            chulbal()
+            time.sleep(600)
+        print('2/3')
+
+        #마감
+        if (now >= "15:30") and (now<="15:35"):
+            magam()
+            return print('출발마감봇 종료')
+
+        print('3/3')
+        i= i+1
+        time.sleep(1)
+        print(str(now)+' '+str(i))
+
+if __name__ == '__main__':
+    timeline()
